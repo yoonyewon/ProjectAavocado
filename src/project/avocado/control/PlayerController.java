1a7e8b90aef5d9ed75a1d54c143d1eba158c0b1d
bangks72 <33773553+bangks72@users.noreply.github.com>
2017-11-28 17:02:05 +0900

    commit

--- src/project/avocado/control/PlayerController.java
+++ src/project/avocado/control/PlayerController.java
@@ -7,26 +7,23 @@ import project.avocado.view.MainView;
 import project.avocado.view.PlayerView;
 
 public class PlayerController implements ActionListener {
-	
+
 	MainView mainview;
 	PlayerView playerview;
-	
+
 	public PlayerController() {
-		
+
 		mainview = new MainView();
 		playerview = new PlayerView();
-		
+
 		eventUp();
-		
+
 	}
-	
-	
-	
-	
+
 	public void eventUp() {
-		
-		//MainView 
-		
+
+		// MainView
+
 		mainview.bt_play.addActionListener(this);
 		mainview.bt_pause.addActionListener(this);
 		mainview.bt_stop.addActionListener(this);
@@ -35,34 +32,46 @@ public class PlayerController implements ActionListener {
 		mainview.bt_login.addActionListener(this);
 		mainview.bt_delete.addActionListener(this);
 		mainview.bt_toMain.addActionListener(this);
-		
-		//PlayerView
-		
+
+		// PlayerView
+
 		playerview.bt_add.addActionListener(this);
 		playerview.bt_add.addActionListener(this);
 		playerview.bt_add.addActionListener(this);
 		playerview.bt_add.addActionListener(this);
 		playerview.bt_add.addActionListener(this);
-		
+
 	}
-	
-
-
-
 
 	@Override
 	public void actionPerformed(ActionEvent mu) {
-		
+
 		Object ob = mu.getSource();
-		
-		if(ob == mainview.bt_play) {
-			
-		} 
-		
+
+		if (ob == mainview.bt_play) {// 재생 관련
+
+		} else if (ob == mainview.bt_pause) {
+
+		} else if (ob == mainview.bt_stop) {
+
+		} else if (ob == mainview.bt_previous) {
+
+		} else if (ob == mainview.bt_next) {
+
+		} else if (ob == mainview.bt_login) {
+
+		} else if (ob == mainview.bt_delete) {
+
+		} else if (ob == mainview.bt_toMain) {
+
+			mainview.setVisible(false);
+
+		}
+
 	}
 
 	public static void main(String[] args) {
 		new PlayerController();
 	}
-	
+
 }
